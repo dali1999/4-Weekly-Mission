c1f1b3f8060b4d4f62abfa7f72d85dd3788dcb0e
Jules <dali1999@naver.com>
2024-04-10 15:50:57 +0900

    feat: Sign Header 추가

--- src/components/sign/SignHeader.jsx
+++ src/components/sign/SignHeader.jsx
@@ -3,13 +3,13 @@ import * as S from "@/components/sign/SignHeader.style";
 import Image from "next/image";
 import logo from "@/assets/svg/logo.svg";
 
-function SignHeader() {
+function SignHeader({ memberText, linkText, link }) {
   return (
     <S.Container>
       <Image src={logo} alt="로고" width={218} />
       <S.HeaderText>
-        <span>회원이 아니신가요? </span>
-        <S.StyledLink href="/signup">회원 가입하기</S.StyledLink>
+        <span>{memberText} </span>
+        <S.StyledLink href={link}>{linkText}</S.StyledLink>
       </S.HeaderText>
     </S.Container>
   );
