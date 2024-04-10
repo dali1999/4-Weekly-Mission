0dab6600bc6e1d7781b1b73d724ad515cbba4775
Jules <dali1999@naver.com>
2024-04-10 15:51:46 +0900

    feat: 에러메시지 상수 관리

---
+++ src/components/sign/ErrorMessages.js
@@ -0,0 +1,16 @@
+const ERROR = {
+  // email
+  EMAIL_REQUIRE: "이메일을 입력해 주세요.",
+  EMAIL_INVALID_PATTERN: "올바른 이메일 주소가 아닙니다.",
+  EMAIL_INVALID_DATA: "이메일을 확인해 주세요.",
+  EMAIL_DUPLICATE: "이미 사용 중인 이메일입니다.",
+
+  // password
+  PASSWORD_REQUIRE: "비밀번호를 입력해 주세요.",
+  PASSWORD_INVALID_PATTERN:
+    "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.",
+  PASSWORD_INVALID_DATA: "비밀번호를 확인해 주세요.",
+  PASSWORD_INVALID_MATCH: "비밀번호가 일치하지 않아요.",
+};
+
+export default ERROR;
